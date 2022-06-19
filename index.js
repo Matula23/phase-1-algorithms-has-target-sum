0a1c55817d4b932e3f6d68698b2b6988a177d59b
Maciej Matula <maciek.matula@gmail.com>
2022-06-19 14:08:12 -0500

    commit tree 0db49caddddad56dc404fee7cbe06cde8b6d1d6f

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+    for(let j = i + 1; j < array.length; j++){
+      console.log(array[i], array[j])
+      if((array[i] + array[j]) === target){
+      return true
+      } 
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
-
 /* 
   Add your pseudocode here
+  iterrate through an array twice
+  if the sum of each itteration equals the input number then return true 
+
 */
 
 /*
   Add written explanation of your solution here
+  Given an array, check to see if any 2 numbers sum up to the input number
 */
 
 // You can run `node index.js` to view these console logs
